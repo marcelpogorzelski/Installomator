bc888e2bf4a17ecb4f15b3f889e2b3025c584ea9
marcelpogorzelski <42753253+marcelpogorzelski@users.noreply.github.com>
2022-03-18 15:49:51 +0100

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -2042,8 +2042,9 @@ diskspace)
 displaylinkmanager)
     name="DisplayLink Manager"
     type="pkg"
-    packageID="com.displaylink.displaylinkmanagerapp"
+    #packageID="com.displaylink.displaylinkmanagerapp"
     downloadURL=https://www.synaptics.com$(redirect=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep 'class="download-link">Download' | head -n 1 | sed 's/.*href="//' | sed 's/".*//') && curl -sfL "https://www.synaptics.com$redirect" | grep Accept | head -n 1 | sed 's/.*href="//' | sed 's/".*//')
+    appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep "Release:" | head -n 1 | cut -d ' ' -f2)
     expectedTeamID="73YQY62QM3"
     ;;
 docker)
